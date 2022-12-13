c176fbcce02b45f5dec52d5be43b9a04daf41e47
T <tomasz.k.tarnowski@gmail.com>
2022-12-13 17:14:15 +0100

    scale position rotation

--- Assets/Scripts/BallComponent.cs
+++ Assets/Scripts/BallComponent.cs
@@ -18,7 +18,7 @@ public class BallComponent : MonoBehaviour
     public float scaleLowerLimit = 1.0f;
 
     public float scaleIncrement = 1.0f;
-    //bool moveForward = true;
+    bool moveForward = true;
     public bool scaleUp = true;
 
 
@@ -32,29 +32,25 @@ public class BallComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //++frames;
-        //Debug.Log("Frames passed =" + frames);
-
-        //framerate = 1 / Time.deltaTime;
-        //Debug.Log("Liczba klatek na sekunde =" + framerate);
-
-        double distance = Math.Sqrt(transform.position.sqrMagnitude);
-
-
-
+        //Scale an rotation
 
         if (vecScale.x <= scaleUpperLimit & scaleUp)
         {                                                   // grow up 
             vecScale.x += scaleIncrement*Time.deltaTime;
             vecScale.y += scaleIncrement*Time.deltaTime;
             transform.localScale = vecScale;
-                       
+
+            vecRotation += Vector3.forward * rotationSpeed;
+            transform.rotation = Quaternion.Euler(vecRotation);
         }
          else if (vecScale.x <= scaleUpperLimit & !scaleUp & vecScale.x>scaleLowerLimit) // grow down 
         {
             vecScale.x -= scaleIncrement * Time.deltaTime;
             vecScale.y -= scaleIncrement * Time.deltaTime;
             transform.localScale = vecScale;
+
+            vecRotation -= Vector3.forward * rotationSpeed;
+            transform.rotation = Quaternion.Euler(vecRotation);
         }
 
         else if (vecScale.x>scaleUpperLimit & scaleUp)      // upper switch
@@ -72,35 +68,39 @@ public class BallComponent : MonoBehaviour
             vecScale.y += scaleIncrement * Time.deltaTime;
             transform.localScale = vecScale;
         }
+
+        // Position 
+
+        double distance = Math.Sqrt(transform.position.sqrMagnitude);
+       
+
+        if (distance <= 3.0 & moveForward)
+        {
+            transform.position += Vector3.up * Time.deltaTime * SpeedY;
+            transform.position += Vector3.left * Time.deltaTime * SpeedX;
+        }
+        else if (distance <= 3.0 & !moveForward)
+        {
+            transform.position -= Vector3.up * Time.deltaTime * SpeedY;
+            transform.position -= Vector3.left * Time.deltaTime * SpeedX;
+        }
+        else if (distance > 3.0 & moveForward)
+        {
+            moveForward = !moveForward;
+            transform.position -= Vector3.up * Time.deltaTime * SpeedY;
+            transform.position -= Vector3.left * Time.deltaTime * SpeedX;
+        }
+        else if (distance > 3.0 & !moveForward)
+        {
+            moveForward = true;
+            transform.position += Vector3.up * Time.deltaTime * SpeedY;
+            transform.position += Vector3.left * Time.deltaTime * SpeedX;
+        }
     }
  
 }
 
-//vecRotation += Vector3.forward * rotationSpeed;
-//transform.rotation = Quaternion.Euler(vecRotation);
 
-//if (distance <= 3.0 & moveForward)
-//{
-//    transform.position += Vector3.up * Time.deltaTime * SpeedY;
-//    transform.position += Vector3.left * Time.deltaTime * SpeedX;
-//}
-//else if (distance <= 3.0 & !moveForward )
-//{
-//    transform.position -= Vector3.up * Time.deltaTime * SpeedY;
-//    transform.position -= Vector3.left * Time.deltaTime * SpeedX;
-//}
-//else if (distance > 3.0 & moveForward)
-//{
-//    moveForward= !moveForward;
-//    transform.position -= Vector3.up * Time.deltaTime * SpeedY;
-//    transform.position -= Vector3.left * Time.deltaTime * SpeedX;
-//}
-//else if (distance > 3.0 & !moveForward)
-//{
-//    moveForward = true;
-//    transform.position += Vector3.up * Time.deltaTime * SpeedY;
-//    transform.position += Vector3.left * Time.deltaTime * SpeedX;
-//}
 
 
 //5.
