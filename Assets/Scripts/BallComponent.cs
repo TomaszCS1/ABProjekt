4d5c06b5b4de14641d21eab66babcee39e97cf43
T <tomasz.k.tarnowski@gmail.com>
2022-12-16 09:12:16 +0100

    2.2. C#. Pętle; Zadanie: ruch

--- Assets/Scripts/BallComponent.cs
+++ Assets/Scripts/BallComponent.cs
@@ -1,153 +1,287 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BallComponent : MonoBehaviour
 {
-    private int frames;
-    private float framerate;
+    //2-------------------------------------
+    //public float SpeedX = 1.0f;
+    //public float SpeedY = 0.75f;
+    public float rotationSpeed = 5f;
+    public Vector3 vecRotation = Vector3.forward;
 
-    public float SpeedX = 1.0f;
-    public float SpeedY = 0.75f;
-    public float rotationSpeed = 1f;
-    public Vector3 vecRotation = Vector3.zero;
-    public Vector3 vecTransform;
+    //public Vector3 vecTransform;
     public Vector2 vecScale;
-    public float scaleUpperLimit = 3.0f;
-    public float scaleLowerLimit = 1.0f;
+    //public float scaleUpperLimit = 3.0f;
+    //public float scaleLowerLimit = 1.0f;
 
-    public float scaleIncrement = 1.0f;
-    bool moveForward = true;
-    public bool scaleUp = true;
+    public float scaleIncrement = 0.8f;
+    //public bool scaleUp = true;
+
+    //3--------------------------------------
+    //cw bonusowe
+    //public enum GameState
+    //{
+    //    Start,
+    //    Pause,
+    //    Exit
+    //}
+    //GameState State = GameState.Start;
+    //--------------------------------------
+
+
+
+    //4------------------------------------------cwiczenie Pętle a tablice i kolekcje
+    public enum BallInstruction
+    {
+        Idle = 0,
+        MoveUp,
+        MoveDown,
+        MoveLeft,
+        MoveRight,
+        ScaleUp,
+        ScaleDown,
+    }
+
+    private int CurrentInstruction = 0;
+
+    //private float TimeInInstruction = 0.0f;
+
+    public float Speed = 1.0f;
+    
+    public List<BallInstruction> Instructions = new List<BallInstruction>();
+
+    private float InstructionLength = 2.0f;
+
+    bool moveUp = true;
+    bool moveLeft;
 
 
     void Start()
     {
-
     }
+
     
+
 
 
     // Update is called once per frame
     void Update()
     {
-        //Scale an rotation
-
-        if (vecScale.x <= scaleUpperLimit & scaleUp)
-        {                                                   // grow up 
-            vecScale.x += scaleIncrement*Time.deltaTime;
-            vecScale.y += scaleIncrement*Time.deltaTime;
-            transform.localScale = vecScale;
-
-            vecRotation += Vector3.forward * rotationSpeed;
-            transform.rotation = Quaternion.Euler(vecRotation);
-        }
-         else if (vecScale.x <= scaleUpperLimit & !scaleUp & vecScale.x>scaleLowerLimit) // grow down 
+    
+        if (CurrentInstruction < Instructions.Count)
         {
-            vecScale.x -= scaleIncrement * Time.deltaTime;
-            vecScale.y -= scaleIncrement * Time.deltaTime;
-            transform.localScale = vecScale;
+            //TimeInInstruction += Time.deltaTime;
+            float RealSpeed = Speed * Time.deltaTime;
+            float distInInstruction = Vector3.Distance(Vector3.zero, transform.position);
 
-            vecRotation -= Vector3.forward * rotationSpeed;
-            transform.rotation = Quaternion.Euler(vecRotation);
+
+            switch (Instructions[CurrentInstruction])
+            {
+                case BallInstruction.MoveUp:
+                    transform.position += Vector3.up * RealSpeed;
+
+                    vecRotation += Vector3.forward * rotationSpeed;
+                    transform.rotation = Quaternion.Euler(vecRotation);
+
+                    vecScale.x += scaleIncrement * Time.deltaTime;
+                    vecScale.y += scaleIncrement * Time.deltaTime;
+                    transform.localScale = vecScale;
+
+                    break;
+
+                case BallInstruction.MoveDown:
+                    transform.position += Vector3.down * RealSpeed;
+
+                    vecRotation += Vector3.back * rotationSpeed;
+                    transform.rotation = Quaternion.Euler(vecRotation);
+
+                    if (vecScale.x >= 1)
+                    {
+                        vecScale.x -= (1/scaleIncrement) * Time.deltaTime;
+                        vecScale.y -= (1/scaleIncrement) * Time.deltaTime;
+                        transform.localScale = vecScale;
+                    }
+
+                    break;
+
+                case BallInstruction.MoveLeft:
+                    transform.position += Vector3.left * RealSpeed;
+
+                    vecRotation += Vector3.forward * rotationSpeed;
+                    transform.rotation = Quaternion.Euler(vecRotation);
+
+                    vecScale.x += scaleIncrement * Time.deltaTime;
+                    vecScale.y += scaleIncrement * Time.deltaTime;
+                    transform.localScale = vecScale;
+                    break;
+
+                case BallInstruction.MoveRight:
+                    transform.position += Vector3.right * RealSpeed;
+
+                    vecRotation += Vector3.back * rotationSpeed;
+                    transform.rotation = Quaternion.Euler(vecRotation);
+
+                    if (vecScale.x >= 1)
+                    {
+                        vecScale.x -= (1 / scaleIncrement) * Time.deltaTime;
+                        vecScale.y -= (1 / scaleIncrement) * Time.deltaTime;
+                        transform.localScale = vecScale;
+                    }
+                    break;
+
+                default:
+                    Debug.Log("Idle");
+                    break;
+            }
+
+            
+
+            if (transform.position.y > InstructionLength & moveUp)                      //move up
+            {
+                ++CurrentInstruction;
+                moveUp=false;
+
+            }
+            else if (transform.position.y<0 & !moveUp & transform.position.x==0)        //move down
+            {
+                ++CurrentInstruction;
+                moveLeft = true;
+            }
+
+            else if (-transform.position.x > InstructionLength & moveLeft)              //move left
+            {
+                ++CurrentInstruction;
+                moveLeft =!moveLeft;
+            }
+
+            else if (transform.position.x> 0 & !moveLeft)                               //move right
+            {
+                transform.position = Vector3.zero;
+                ++CurrentInstruction;
+
+            }
+
+
+
         }
 
-        else if (vecScale.x>scaleUpperLimit & scaleUp)      // upper switch
-        {
-            scaleUp = !scaleUp;
-            vecScale.x -= scaleIncrement * Time.deltaTime;
-            vecScale.y -= scaleIncrement * Time.deltaTime;
-            transform.localScale = vecScale;
-        }
 
-        else if (vecScale.x < scaleLowerLimit & !scaleUp)   //lower switch
-        {
-            scaleUp = !scaleUp;
-            vecScale.x += scaleIncrement * Time.deltaTime;
-            vecScale.y += scaleIncrement * Time.deltaTime;
-            transform.localScale = vecScale;
-        }
+        //4--------------------------------------cwiczenie Petle a tablice i kolekcje
+        //switch (Instruction)
+        //{
+        //    case BallInstruction.MoveUp:
+        //        transform.position += new Vector3(0, 1, 0) * Speed * Time.deltaTime;
+        //        break;
 
-        // Position 
+        //    case BallInstruction.MoveDown:
+        //        transform.position -= new Vector3(0, 1, 0) * Speed * Time.deltaTime;
+        //        break;
 
-        double distance = Math.Sqrt(transform.position.sqrMagnitude);
-       
+        //    case BallInstruction.MoveLeft:
+        //        transform.position -= new Vector3(1, 0, 0) * Speed * Time.deltaTime;
+        //        break;
 
-        if (distance <= 3.0 & moveForward)
-        {
-            transform.position += Vector3.up * Time.deltaTime * SpeedY;
-            transform.position += Vector3.left * Time.deltaTime * SpeedX;
-        }
-        else if (distance <= 3.0 & !moveForward)
-        {
-            transform.position -= Vector3.up * Time.deltaTime * SpeedY;
-            transform.position -= Vector3.left * Time.deltaTime * SpeedX;
-        }
-        else if (distance > 3.0 & moveForward)
-        {
-            moveForward = !moveForward;
-            transform.position -= Vector3.up * Time.deltaTime * SpeedY;
-            transform.position -= Vector3.left * Time.deltaTime * SpeedX;
-        }
-        else if (distance > 3.0 & !moveForward)
-        {
-            moveForward = true;
-            transform.position += Vector3.up * Time.deltaTime * SpeedY;
-            transform.position += Vector3.left * Time.deltaTime * SpeedX;
-        }
+        //    case BallInstruction.MoveRight:
+        //        transform.position += new Vector3(1, 0, 0) * Speed * Time.deltaTime;
+        //        break;
+
+        //    case BallInstruction.ScaleUp:
+        //        transform.localScale += new Vector3(1,1,0) *scaleIncrement* Time.deltaTime;
+        //        break;
+
+
+        //    case BallInstruction.ScaleDown:
+        //        transform.localScale -= new Vector3(1, 1, 0) * scaleIncrement * Time.deltaTime;
+        //        break;
+
+        //    default:
+        //        Debug.Log("Idle");
+        //        break;
+        //}
+
+
+
+
+        //3--------------------------------------
+        //cw bonusowe
+        //Debug.Log("State: " + State);
+
+        //int StateVal = (int)State;
+        //++StateVal;
+        //State = (GameState)StateVal;
+        //Debug.Log("New State: " + State);
+
+
+        //2-----------------------------------------------------------------------------------
+        //Scale and rotation
+
+        //    if (vecScale.x <= scaleUpperLimit & scaleUp)
+        //    {                                                   // grow up 
+        //        vecScale.x += scaleIncrement*Time.deltaTime;
+        //        vecScale.y += scaleIncrement*Time.deltaTime;
+        //        transform.localScale = vecScale;
+
+        //        vecRotation += Vector3.forward * rotationSpeed;
+        //        transform.rotation = Quaternion.Euler(vecRotation);
+        //    }
+        //     else if (vecScale.x <= scaleUpperLimit & !scaleUp & vecScale.x>scaleLowerLimit) // grow down 
+        //    {
+        //        vecScale.x -= scaleIncrement * Time.deltaTime;
+        //        vecScale.y -= scaleIncrement * Time.deltaTime;
+        //        transform.localScale = vecScale;
+
+        //        vecRotation -= Vector3.forward * rotationSpeed;
+        //        transform.rotation = Quaternion.Euler(vecRotation);
+        //    }
+
+        //    else if (vecScale.x>scaleUpperLimit & scaleUp)      // upper switch
+        //    {
+        //        scaleUp = !scaleUp;
+        //        vecScale.x -= scaleIncrement * Time.deltaTime;
+        //        vecScale.y -= scaleIncrement * Time.deltaTime;
+        //        transform.localScale = vecScale;
+        //    }
+
+        //    else if (vecScale.x < scaleLowerLimit & !scaleUp)   //lower switch
+        //    {
+        //        scaleUp = !scaleUp;
+        //        vecScale.x += scaleIncrement * Time.deltaTime;
+        //        vecScale.y += scaleIncrement * Time.deltaTime;
+        //        transform.localScale = vecScale;
+        //    }
+
+        //    // Position 
+
+        //    double distance = Math.Sqrt(transform.position.sqrMagnitude);
+
+
+        //    if (distance <= 3.0 & moveForward)
+        //    {
+        //        transform.position += Vector3.up * Time.deltaTime * SpeedY;
+        //        transform.position += Vector3.left * Time.deltaTime * SpeedX;
+        //    }
+        //    else if (distance <= 3.0 & !moveForward)
+        //    {
+        //        transform.position -= Vector3.up * Time.deltaTime * SpeedY;
+        //        transform.position -= Vector3.left * Time.deltaTime * SpeedX;
+        //    }
+        //    else if (distance > 3.0 & moveForward)
+        //    {
+        //        moveForward = !moveForward;
+        //        transform.position -= Vector3.up * Time.deltaTime * SpeedY;
+        //        transform.position -= Vector3.left * Time.deltaTime * SpeedX;
+        //    }
+        //    else if (distance > 3.0 & !moveForward)
+        //    {
+        //        moveForward = true;
+        //        transform.position += Vector3.up * Time.deltaTime * SpeedY;
+        //        transform.position += Vector3.left * Time.deltaTime * SpeedX;
+        //    }
+        //-----------------------------------------------------------------------------------
+
+
     }
- 
+
 }
-
-
-
-
-//5.
-//if (vecScale.x <= scaleUpperLimit)
-//{
-//    vecScale.x += scaleIncrement;
-//    vecScale.y += scaleIncrement;
-//    vecScale.z += scaleIncrement;
-//    transform.localScale = vecScale;
-//}
-
-
-//4.
-//if (transform.localScale.x <= scaleUpperLimit)
-//{
-//    transform.localScale = transform.localScale + new Vector3(scaleIncrement, scaleIncrement, scaleIncrement);
-
-
-//3.
-//while (vecScale.x <= scaleUpperLimit)
-//{
-//    vecScale.x += scaleIncrement;
-//    vecScale.y += scaleIncrement;
-//    vecScale.z += scaleIncrement;
-//    transform.localScale = vecScale;
-//};
-
-//2.
-//do
-//{
-//    vecScale.x += scaleIncrement;
-//    vecScale.y += scaleIncrement;
-//    vecScale.z += scaleIncrement;
-//    transform.localScale = vecScale;
-//}
-//while (vecScale.x >= 3) ;
-
-//1.
-//do
-//{
-
-//    transform.localScale = vecScale * (1 + scaleIncrement);
-//    vecScale = vecScale * (1 + scaleIncrement);
-
-//}
-//while (vecScale.x < scaleUpperLimit);
-
-
-
-
