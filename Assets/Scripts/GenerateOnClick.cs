2b49dcb498518bfe773fb456a94ebb8fd279ed95
T <tomasz.k.tarnowski@gmail.com>
2023-01-03 21:58:49 +0100

    changes

--- Assets/Scripts/GenerateOnClick.cs
+++ Assets/Scripts/GenerateOnClick.cs
@@ -1,22 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-    public class GenerateOnClick : MonoBehaviour
-    {
-        public GameObject objectToCopy;
-        public Transform generationPoint;
+public class GenerateOnClick : MonoBehaviour
+//{
+//    public GameObject original;  // drag the original gameobject into this field in the inspector
+//    public GameObject copy;      // drag the prefab for the copy into this field in the inspector
 
-        private SpringJoint2D originalSpringJoint;
+//    public void OnPointerClick(PointerEventData eventData)
+//    {
+//        Instantiate a copy of the original gameobject
+//       GameObject newCopy = Instantiate(copy, original.transform.position, original.transform.rotation);
 
-        private Rigidbody2D m_rigidbody;                // RigidBody dla kulki
-        private SpringJoint2D m_connectedJoint;         // field do kontrolowania polaczenia sprezynowego w kuli
-        private Rigidbody2D m_connectedBody;            // RigidBody dla punktu sprezynowego kulki
+//        Apply the same Spring Joint 2D component to the new copy as on the original
+//        SpringJoint2D springJoint = original.GetComponent<SpringJoint2D>();
+//        newCopy.AddComponent<SpringJoint2D>().connectedAnchor = springJoint.connectedAnchor;
+//        newCopy.GetComponent<SpringJoint2D>().distance = springJoint.distance;
+//        newCopy.GetComponent<SpringJoint2D>().dampingRatio = springJoint.dampingRatio;
+//        newCopy.GetComponent<SpringJoint2D>().frequency = springJoint.frequency;
+//    }
+//}
+
+
+
+
+
+{
+    public GameObject objectToCopy;
+    public Transform generationPoint;
+
+    private SpringJoint2D originalSpringJoint;
+
+    private Rigidbody2D m_rigidbody;                // RigidBody dla kulki
+    private SpringJoint2D m_connectedJoint;         // field do kontrolowania polaczenia sprezynowego w kuli
+    private Rigidbody2D m_connectedBody;            // RigidBody dla punktu sprezynowego kulki
 
     private void Start()
     {
         // Get the Spring Joint 2D component on the original GameObject
-         originalSpringJoint = objectToCopy.GetComponent<SpringJoint2D>();
+        originalSpringJoint = objectToCopy.GetComponent<SpringJoint2D>();
     }
 
     void Update()
@@ -30,7 +53,7 @@ using UnityEngine;
                 GameObject copy = Instantiate(objectToCopy, generationPoint.position, Quaternion.identity);
                 copy.name = objectToCopy.name;
 
-              
+
 
                 // Add a Spring Joint 2D component to the copy
                 SpringJoint2D copySpringJoint = copy.AddComponent<SpringJoint2D>();
