ea2541b742b3d19098ed2873c3b1e066b6ea7004
T <tomasz.k.tarnowski@gmail.com>
2023-01-13 11:27:21 +0100

    2 methods removed

--- Assets/Scripts/InteractiveComponent.cs
+++ Assets/Scripts/InteractiveComponent.cs
@@ -9,9 +9,7 @@ public class InteractiveComponent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //// subscribes Events from GameplayManager
-        //GameplayManager.OnGamePaused += DoPause;
-        //GameplayManager.OnGamePlaying += DoPlay;
+        
     }
 
     // Update is called once per frame
@@ -24,11 +22,6 @@ public class InteractiveComponent : MonoBehaviour
     {}
 
 
-    private void DoPlay()
-    {}
-
-
-    private void DoPause()
-    {}
+   
 
 }
